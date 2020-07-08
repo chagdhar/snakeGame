19bc0624e63fdbf030136ef155f06de352330c10
Chandu <chandukiran13@gmail.com>
2020-07-08 20:32:42 +0530

    time

--- trial.py
+++ trial.py
@@ -1,5 +1,8 @@
 import pygame
+import time
+import numpy as np
 pygame.init()
 gd = pygame.display.set_mode((800,800))
+gd.fill([255,255,255])
 pygame.display.set_caption("Snake")
 clk = pygame.time.CLock()
