b1865322b6f0adc65023ed22f385d16f0982eb2e
Chandu <chandukiran13@gmail.com>
2020-07-08 21:15:27 +0530

    circle

--- trial.py
+++ trial.py
@@ -6,7 +6,10 @@ gd = pygame.display.set_mode((600,600))
 gd.fill([255,255,255])
 pygame.display.set_caption("Snake")
 clk = pygame.time.Clock()
+pygame.draw.circle(gd,[205,0,205],(300,300),3)
 crush = True
+def grid(a,b):
+    return(a*6,b*6)
 while crush:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
