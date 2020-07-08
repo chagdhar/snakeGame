860f8ae19243a9dddb01cdf0bad000d80a8c843b
Chandu <chandukiran13@gmail.com>
2020-07-08 21:01:57 +0530

    window_open

--- trial.py
+++ trial.py
@@ -2,7 +2,16 @@ import pygame
 import time
 import numpy as np
 pygame.init()
-gd = pygame.display.set_mode((800,800))
+gd = pygame.display.set_mode((600,600))
 gd.fill([255,255,255])
 pygame.display.set_caption("Snake")
-clk = pygame.time.CLock()
+clk = pygame.time.Clock()
+crush = True
+while crush:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crush = False
+        
+    pygame.display.update()
+    clk.tick(60)
+pygame.quit()
