caca64c2d8df15341375c59c22270baad3953fc1
Chandu Kiran <chandukiran13@gmail.com>
2020-07-09 22:14:21 +0530

    spaces to tabs

--- trial.py
+++ trial.py
@@ -15,90 +15,90 @@ dot_img = pygame.image.load("dot.png")
 font = pygame.font.Font('freesansbold.ttf', 10)
 len_snake = 0
 def grid(a):
-    return(tuple(t*6 for t in a))
+	return(tuple(t*6 for t in a))
 def reset():
-    global dot, dot_loc,head,vel,snake,check,len_snake
-    dot = False
-    dot_loc = ()
-    len_snake = 3
-    vel = ""
-    head=(1,1)
-    snake = [head]
-    check = 0
+	global dot, dot_loc,head,vel,snake,check,len_snake
+	dot = False
+	dot_loc = ()
+	len_snake = 3
+	vel = ""
+	head=(1,1)
+	snake = [head]
+	check = 0
 reset()
 while crush:
-    gd.fill([255,255,255])
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crush = False
-        if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-            if vel == "":
-                if event.key == pygame.K_LEFT:
-                    vel = "Left"
-                if event.key == pygame.K_RIGHT:
-                    vel = "Right"
-                if event.key == pygame.K_UP:
-                    vel = "Up"
-                if event.key == pygame.K_DOWN:
-                    vel = "Down"
-            if vel =="Up":
-                if event.key == pygame.K_LEFT:
-                    vel = "Left"
-                if event.key == pygame.K_RIGHT:
-                    vel = "Right"
-            if vel == "Down":
-                if event.key == pygame.K_LEFT:
-                    vel = "Left"
-                if event.key == pygame.K_RIGHT:
-                    vel = "Right"
-            if vel == "Right":
-                if event.key == pygame.K_DOWN:
-                    vel = "Down"
-                if event.key == pygame.K_UP:
-                    vel = "Up"
-            if vel == "Left":
-                if event.key == pygame.K_UP:
-                    vel = "Up"
-                if event.key == pygame.K_DOWN:
-                    vel = "Down"
-    a,b = head
-    if vel == "Down":
-        b+=1
-    if vel == "Up":
-        b-=1
-    if vel == "Right":
-        a+=1
-    if vel == "Left":
-        a-=1
-    head = (a%100,b%100)
-    snake.append(head)
-    if len(snake)>len_snake:
-        snake = snake[1:]
-    if not dot:
-        difflis = [i for i in lis if i not in snake]
-        ndin = rn.randint(0,len(difflis))
-        dot_loc = difflis[ndin]
-        dot = True
-    else:
-        gd.blit(dot_img,grid(dot_loc))
-        if head == dot_loc:
-            len_snake+=1
-            dot = False
-    for a in snake:
-        if head == a:
-            gd.blit(head_img,grid(a))
-        else:
-            gd.blit(body_img,grid(a))
-    for a in snake:
-        if a == head:
-            check+=1
-    if check>1:
-        reset()
-    check =0
-    text = font.render("score = "+str(len_snake-3),True,(0,0,0),(255,255,255))
-    textrec = text.get_rect()
-    textrec.center = (570,10)
-    gd.blit(text,textrec)
-    pygame.display.update()
-    clk.tick(10)
+	gd.fill([255,255,255])
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			crush = False
+		if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+			if vel == "":
+				if event.key == pygame.K_LEFT:
+					vel = "Left"
+				if event.key == pygame.K_RIGHT:
+					vel = "Right"
+				if event.key == pygame.K_UP:
+					vel = "Up"
+				if event.key == pygame.K_DOWN:
+					vel = "Down"
+			if vel =="Up":
+				if event.key == pygame.K_LEFT:
+					vel = "Left"
+				if event.key == pygame.K_RIGHT:
+					vel = "Right"
+			if vel == "Down":
+				if event.key == pygame.K_LEFT:
+					vel = "Left"
+				if event.key == pygame.K_RIGHT:
+					vel = "Right"
+			if vel == "Right":
+				if event.key == pygame.K_DOWN:
+					vel = "Down"
+				if event.key == pygame.K_UP:
+					vel = "Up"
+			if vel == "Left":
+				if event.key == pygame.K_UP:
+					vel = "Up"
+				if event.key == pygame.K_DOWN:
+					vel = "Down"
+	a,b = head
+	if vel == "Down":
+		b+=1
+	if vel == "Up":
+		b-=1
+	if vel == "Right":
+		a+=1
+	if vel == "Left":
+		a-=1
+	head = (a%100,b%100)
+	snake.append(head)
+	if len(snake)>len_snake:
+		snake = snake[1:]
+	if not dot:
+		difflis = [i for i in lis if i not in snake]
+		ndin = rn.randint(0,len(difflis))
+		dot_loc = difflis[ndin]
+		dot = True
+	else:
+		gd.blit(dot_img,grid(dot_loc))
+		if head == dot_loc:
+			len_snake+=1
+			dot = False
+	for a in snake:
+		if head == a:
+			gd.blit(head_img,grid(a))
+		else:
+			gd.blit(body_img,grid(a))
+	for a in snake:
+		if a == head:
+			check+=1
+	if check>1:
+		reset()
+	check =0
+	text = font.render("score = "+str(len_snake-3),True,(0,0,0),(255,255,255))
+	textrec = text.get_rect()
+	textrec.center = (570,10)
+	gd.blit(text,textrec)
+	pygame.display.update()
+	clk.tick(10)
 pygame.quit()
