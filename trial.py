4abee917792027b3db9528fe9eaabb34df121dcb
Chandu Kiran <chandukiran13@gmail.com>
2020-07-09 13:16:26 +0530

    working_snake

--- trial.py
+++ trial.py
@@ -9,36 +9,57 @@ pygame.display.set_caption("Snake")
 clk = pygame.time.Clock()
 crush = True
 dot = False
+len_snake = 5
+head_img = pygame.image.load("head.png")
+body_img = pygame.image.load("point.png")
 head = (1,1)
-vel = "Down"
+vel = ""
 snake = [head]
 def grid(a,b):
     return((a*6,b*6))
 def snake_update():
     global snake, head
-    snake =[head]
+    snake.append(head)
+    if len(snake)>len_snake:
+        snake = snake[1:]
 def play():
     global crush, clk, snake, head, vel
     while crush:
+        gd.fill([255,255,255])
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 crush = False
             if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    vel = "Up"
-                if event.key == pygame.K_DOWN:
-                    vel = "Down"
-                if event.key == pygame.K_LEFT:
-                    vel = "Left"
-                if event.key == pygame.K_RIGHT:
-                    vel = "Right"
+                if vel =="Up" or vel == "":
+                    if event.key == pygame.K_LEFT:
+                        vel = "Left"
+                    if event.key == pygame.K_RIGHT:
+                        vel = "Right"
+                if vel == "Down" or vel == "":
+                    if event.key == pygame.K_LEFT:
+                        vel = "Left"
+                    if event.key == pygame.K_RIGHT:
+                        vel = "Right"
+                if vel == "Right" or vel == "":
+                    if event.key == pygame.K_DOWN:
+                        vel = "Down"
+                    if event.key == pygame.K_UP:
+                        vel = "Up"
+                if vel == "Left" or vel == "":
+                    if event.key == pygame.K_UP:
+                        vel = "Up"
+                    if event.key == pygame.K_DOWN:
+                        vel = "Down"
         snake_mov()
         snake_update()
         for a in snake:
             x,y = a
-            pygame.draw.circle(gd,[205,0,205],grid(x,y),3)
+            if head == a:
+                gd.blit(head_img,grid(x,y))
+            else:
+                gd.blit(body_img,grid(x,y))
         pygame.display.update()
-        clk.tick(10)
+        clk.tick(5)
 def snake_mov():
     global head, vel
     a,b = head
@@ -51,5 +72,6 @@ def snake_mov():
     if vel == "Left":
         a-=1
     head = (a%100,b%100)
+
 play()
 pygame.quit()
