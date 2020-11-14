eb1c4e3def32214b3eb668b59b2212e85e0aec9a
Chandu Kiran <chandukiran13@gmail.com>
2020-11-14 09:54:42 +0530

    Update New.py

--- New.py
+++ New.py
@@ -8,3 +8,13 @@ font = pygame.font.Font('freesansbold.ttf', 10)
 segments_at_head_loc = 2
 def grid(a):
     return tuple(t*6 for t in a)
+while crush:
+    if segments_at_head_loc>1:
+        dot = False
+        dot_loc = ()
+        len_snake = 3
+        x_speed = 0
+        y_speed = 0
+        head=(1,1)
+        snake = [head]
+    segments_at_head_loc =0
